d78decda3a7ebc29efbac672d493e3b9da2348a6
Rodrigo Ruiz <rodrigo.ruiz@deskmanager.app>
2023-06-12 01:21:31 -0600

    se agregaron onChanes en los componentes input y select

--- components/form/Input.js
+++ components/form/Input.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Input(props) {
   {name='', placeholder = '', value = '', type, onChange, disabled } = props
+
+  const _onChange =(value) => {if(value) onChange(value)}
   return (
 
     <div>
       {name ? name : placeholder}
-      <input value={value} type={type} onChange={onChange} placeholder={placeholder}>
-      
-      </input>
+      <input value={value} type={type} onChange={_onChange} placeholder={placeholder} />
 
 
     </div>
--- components/form/Select.js
+++ components/form/Select.js
@@ -3,9 +3,14 @@ import React from 'react'
 function Select(props) {
 
   {name = '', placeholder, required, value = '', onChange, disabled, options= []} = props 
+
+
+  const _onChange =(value) => {
+    if(value) onChange(value) 
+  }
   return (
     <div>
-      <select>
+      <select id={_onChange}>
         {options.map((item, index) => (<option key={index} value={item.value}> {item.name}
         </option>))}
       </select>
